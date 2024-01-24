2199590a55dff04084801311c9045d9b7d3c35fb
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2024-01-24 09:19:35 +0600

    Final ShapeFactory.java

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,4 +1,23 @@
 import java.util.Scanner;
+interface Shape {
+    void draw();
+    }
+
+    public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+    System.out.println("Inside Rectangle::draw() method.");
+        }
+    }
+    
+    public class Square implements Shape {
+    @Override
+    public void draw() {
+    System.out.println("Inside Square::draw() method.");
+        }
+    }
+    
+
 
 class ShapeFactory{
     private static ShapeFactory instance;
@@ -31,11 +50,17 @@ class ShapeFactory{
 public class FactoryPatternDemo {
     public static void main(String[] args) {
     ShapeFactory shapeFactory;
+    
     Shape shape1 = shapeFactory.getShape("CIRCLE");
+    
     shape1.draw();
+  
     Shape shape2 = shapeFactory.getShape("RECTANGLE");
+ 
     shape2.draw();
+ 
     Shape shape3 = shapeFactory.getShape("SQUARE");
+
     shape3.draw();
     }
 }
